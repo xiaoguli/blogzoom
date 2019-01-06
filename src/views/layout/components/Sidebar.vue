4fa94d5c92ed0d5baef530974fb1dd62170cf852
guyun <gunyun@kelezhibo.cn>
2019-01-06 18:38:31 +0800

    put

---
+++ src/views/layout/components/Sidebar.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <el-card shadow="never">
+            <el-menu :default-active="active" @select="onSelect">
+                <el-menu-item v-for="item in constantRouterMap" v-if="item.meta&&item.meta.type=='user'&&(token||!item.meta.LoginRequired)&&(!mini||!item.meta.mini)"
+                    :key="item.path" :index="item.path">
+                    <i :class="item.meta.icon"></i>
+                    <span slot="title">{{item.meta.title}}</span>
+                </el-menu-item>
+            </el-menu>
+        </el-card>
+
+        <el-card shadow="never" style="margin-top: 20px;text-align: center">
+            <div v-if="!token" style="font-size: 0.9rem;line-height: 1.5;color: #606c71;">
+                <el-tag type="danger" size="small">&nbsp;</el-tag>&nbsp;&nbsp; NO&nbsp;&nbsp;
+                <el-button type="text" @click="openTokenDialog">登录</el-button>
+            </div>
+            <div v-if="token" style="font-size: 0.9rem;line-height: 1.5;color: #303133;">
+                <el-tag type="success" size="small">&nbsp;</el-tag>&nbsp;&nbsp; YES&nbsp;&nbsp;
+                <el-button type="text" @click="cancellation">注销</el-button>
+            </div>
+        </el-card>
+        <token-dialog ref="tokenDialog"></token-dialog>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex'
+    import { constantRouterMap } from '@/router'
+    import TokenDialog from '@/views/common/TokenDialog'
+    export default {
+        components: {
+            TokenDialog
+        },
+        data() {
+            return {
+                constantRouterMap,
+                active: "",
+                parentUrl: "",
+                menuList: []
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'token',
+                'githubUsername',
+                'mini'
+            ])
+        },
+        mounted() {
+            let arr = this.$route.path.split("/")
+            this.active = "/" + arr[1] + "/" + arr[2]
+        },
+        methods: {
+            onSelect(index) {
+                this.$router.push(index)
+            },
+            openTokenDialog() {
+                this.$refs.tokenDialog.open(() => {
+           
+                })
+            },
+            cancellation() {
+                this.$store.dispatch("Cancellation")
+            }
+        }
+    }
+</script>
